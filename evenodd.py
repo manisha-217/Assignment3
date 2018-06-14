a25d12e712e0fc8089511240e28c258f3e3852b6
manisha-217 <manishakumari993424@gmail.com>
2018-06-13 23:51:30 -0700

    assingment 3rd added

---
+++ evenodd.py
@@ -0,0 +1,6 @@
+num=int(input("enter a value"))
+if (num%2)==0:
+  print("number is even".format(num))
+
+else :
+  print("no is odd".format(num))
---
+++ stack.py
@@ -0,0 +1,17 @@
+x=[]
+x.append(input("enter 1st no x"))
+x.append(input("enter 2nd no x"))
+x.append(input("enter 3rd no x"))
+print(x)
+x.pop()
+print(x)
+print("\n")
+
+y=[]
+y.insert(0,input("enter first value y"))
+y.insert(0,input("enter second value y"))
+y.insert(0,input("enter third value y"))
+y.insert(0,input("enter fourth value y"))
+print(y)
+del y[0]
+print(y)
